b87ad4b3e1f52181d42a04d6218a4fbdb7145351
AmidAima <136964886+AmidAima@users.noreply.github.com>
2023-07-31 20:55:03 +0300

    Update rechercher.php

--- Commerce/commandes/rechercher.php
+++ Commerce/commandes/rechercher.php
@@ -5,17 +5,17 @@ if (isset($_POST['recherche'])) {
  
     include("../connexion.php");
     // Requête de recherche des médicaments correspondants
-    $sql = "SELECT * FROM commandes WHERE id_client LIKE '%$search_term%' OR moyen_livraison LIKE '%$search_term%'  OR date_commande LIKE '%$search_term%'";
+    $sql = "SELECT * FROM commandes WHERE id_utilisateur LIKE '%$search_term%' OR moyen_livraison LIKE '%$search_term%'  OR date_commande LIKE '%$search_term%'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         echo "<h2>Résultats de recherche pour \"$search_term\" :</h2>";
         echo "<table border=1>";
-        echo "<tr><th>Id</th><th>Id client</th><th>Date commande</th><th>moyen livraison</th><th>modifier</th><th>supprimer</th></tr>";
+        echo "<tr><th>Id</th><th>Id utilisateur</th><th>Date commande</th><th>moyen livraison</th><th>modifier</th><th>supprimer</th></tr>";
         while ($row = $result->fetch_assoc()) {
             echo "<tr>";
             echo "<td>" . $row['id_commande'] . "</td>";
-            echo "<td>" . $row['id_client'] . "</td>";
+            echo "<td>" . $row['id_utilisateur'] . "</td>";
             echo "<td>" . $row['date_commande'] . "</td>";
             echo "<td>" . $row['moyen_livraison'] . "</td>";
             echo "<td><a href='modifier.php?id_commande=".$row['id_commande']."'>Modifier</a></td>";
