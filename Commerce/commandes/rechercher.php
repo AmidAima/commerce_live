7b182349ff4149a851a6b98e1f5b9c62d777c191
AmidAima <136964886+AmidAima@users.noreply.github.com>
2023-07-31 15:08:56 +0300

    Add files via upload

---
+++ Commerce/commandes/rechercher.php
@@ -0,0 +1,46 @@
+<?php
+// Recherche de médicaments
+if (isset($_POST['recherche'])) {
+    $search_term = $_POST['recherche'];
+ 
+    include("../connexion.php");
+    // Requête de recherche des médicaments correspondants
+    $sql = "SELECT * FROM commandes WHERE id_client LIKE '%$search_term%' OR moyen_livraison LIKE '%$search_term%'  OR date_commande LIKE '%$search_term%'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<h2>Résultats de recherche pour \"$search_term\" :</h2>";
+        echo "<table border=1>";
+        echo "<tr><th>Id</th><th>Id client</th><th>Date commande</th><th>moyen livraison</th><th>modifier</th><th>supprimer</th></tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['id_commande'] . "</td>";
+            echo "<td>" . $row['id_client'] . "</td>";
+            echo "<td>" . $row['date_commande'] . "</td>";
+            echo "<td>" . $row['moyen_livraison'] . "</td>";
+            echo "<td><a href='modifier.php?id_commande=".$row['id_commande']."'>Modifier</a></td>";
+            echo "<td><a href='supprimer.php?id_commande=".$row['id_commande']."'>Supprimer</a></td>";
+            echo "</tr>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Aucun résultat trouvé pour \"$search_term\".";
+    }
+
+    $conn->close();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../home.css" class="css">
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Commerce/commandes/supprimer.php
@@ -0,0 +1,64 @@
+<?php
+include("../connexion.php");
+
+// Vérifier si la clé 'code' existe dans $_GET
+if (isset($_GET['id_commande'])) {
+    $id_commande = $_GET['id_commande'];
+
+    $sql = "DELETE FROM commandes WHERE id_commande= '$id_commande'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Le produit a été supprimé avec succès.";
+    } else {
+        echo "Erreur lors de la suppression du produit : " . $conn->error;
+    }
+} else {
+    echo "La clé  n'existe pas dans la requête GET.";
+}
+
+$conn->close();
+
+include("../connexion.php");
+
+$sql = "SELECT * FROM commandes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table border=1>";
+    echo "<tr><th>ID</th><th>Date commande</th><th>Id client</th><th>Moyen de livraison</th><th>Modifier</th><th>supprimer</th></tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$row['id_commande']."</td>";
+        echo "<td>" . $row['date_commande'] . "</td>";
+        echo "<td>" . $row['id_client'] . "</td>";
+        echo "<td>" . $row['moyen_livraison'] . "</td>";
+      ;
+        echo "<td><a href='modifier.php?id_commande=".$row['id_commande']."'>Modifier</a></td>";
+        echo "<td><a href='supprimer.php?id_commande=".$row['id_commande']."'>Supprimer</a></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+   
+
+} else {
+    echo "Aucun produit enregistré dans la base de données.";
+}
+
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commandes</title>
+    <link rel="stylesheet" type="text/css" href="../home.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
