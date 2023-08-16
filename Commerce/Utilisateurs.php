b3728727ec17d2a203bc0f28b429cb427f4419c6
AmidAima <136964886+AmidAima@users.noreply.github.com>
2023-08-16 10:55:15 +0300

    Update Utilisateurs.php

--- Commerce/Utilisateurs.php
+++ Commerce/Utilisateurs.php
@@ -26,7 +26,8 @@
     <ul> 
        <li class="dropdown">
          <a href="#" class="dropbtn">Facture</a ><div class="dropdown-content">
-          <a href="">Afficher facture</a>
+          <a href="./facture/affichage.php">Afficher facture</a>
+          <a href="./facture/ajout.php">ajouter liste facture</a>
         </div>
       </li>
     </ul >  
@@ -34,25 +35,36 @@
       <li class="dropdown">
         <a href="#" class="dropbtn">Commandes</a ><div class="dropdown-content">
           <a href="./commandes/affichage.php">Commandes des client</a>
-          <a href="./commandes/details.php">Details commandes</a>
+          
           <a href="./commandes/ajout.php">Ajouter des commandes</a>
         </div>
       </li>
-    </ul > </div>
+    </ul >
+    <ul>
+       <li class="dropdown">
+       <a href="#" class="dropbtn">Details commandes</a ><div class="dropdown-content">
+         <a href="./detail/details.php">ajout details</a>
+         <a href="./detail/affichage.php">details commandes</a>
+       </li>  
+    </ul>
+   <ul><li class="dropdown">
+   <a href="#" class="dropbtn">Deconnecter</a><div class="dropdown-content">
+     <a href="destroy.php">vous voulez deconnecter?</a></li></ul>
+    </div>
   <aside class="right-aside">
     <h3>Qu'est ce que vous voulez ici?</h3 >
     <ol>
       
-   <li><p>veux tu voir tous les produits electroniques?<a href="./produits/afichage.php"> tout est ici</a></p></li>
-   <li><p>Veux tu voir tous les fournisseurs des notre produits electroniques?<a href="./fournisseurs/affichage.php"> Clique ici</a></p></li>
-   <li><p>veux tu afficher les factures?<a href="./factures/affichage.php"> Clique ici</a></p></li>
-   <li><p>veux tu voir tous les  commandes des clients?<a href="./commandes/affichage.php"> Clique ici</a></p></li>
+   <li><p>veux tu voir tous les produits electroniques?<a href="./produits/afichage.php"> clique ici</a></p></li>
+   <li><p>Veux tu voir tous les fournisseurs des notre produits electroniques?<a href="./fournisseurs/affichage.php">clique ici</a></p></li>
+   <li><p>veux tu afficher les factures?<a href="./facture/affichage.php"> clique ici</a></p></li>
+   <li><p>veux tu voir tous les  commandes des clients?<a href="./commandes/affichage.php"> clique ici</a></p></li>
    
   </ol>
   <ul>
     <li><h4>Cette information peut satisfaire votre besoin ?</h4></li>
   </ul>
   </aside>
-  
+  <!-- Reste du code... -->
 </body>
 </html>
